ab754de3953c2be0003df165fc3e6c535267ffbc
Dot <madnauk@gmail.com>
2021-12-28 05:11:50 +0300

    Initial commit

---
+++ botnest.py
@@ -0,0 +1,22 @@
+import discord
+import logging
+import json
+
+logging.basicConfig(level=logging.INFO)
+
+with open("config.json") as f:
+    config = json.load(f)
+
+token = config["discord_token"]
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print("Logged on as {0}!".format(self.user))
+
+    async def on_message(self, message):
+        print("Message from {0.author}: {0.content}".format(message))
+
+
+client = MyClient()
+client.run(token)
